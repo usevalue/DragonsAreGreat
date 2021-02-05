38aa30a3c06e04eb8f29f6890c4cc226b1fab2f4
usevalue <bercilac@gmail.com>
2021-02-05 17:49:35 -0500

    View counter

--- server/index.js
+++ server/index.js
@@ -2,24 +2,33 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname,'../client')
-const staticPath = path.join(clientPath,'/static');
-const viewsPath = path.join(clientPath,'/views')
+const clientPath = path.join(__dirname,'../client/')
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath = path.join(clientPath,'/views/')
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
 
+var x = 0;
+
+const counter = function(req, res, next) {
+    x++;
+    console.log(x);
+    next();
+}
+
+app.use(express.static(staticPath));
+
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/famous', function(req, res) {
-    res.render('famous');
+app.get('/famous', counter, function(req, res) {
+    res.render('famous',{count: x});
 });
 
-app.use(express.static(staticPath));
 
 
 app.listen(2000);
