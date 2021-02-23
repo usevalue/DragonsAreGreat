86fa183d90754e034d7619432892bf2ddc17b4af
usevalue <bercilac@gmail.com>
2021-02-23 10:23:31 -0500

    Copying tutorial 3

---
+++ tutorial4/server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const ejs = require ('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Navigation
+
+const clientPath = path.join(__dirname,'../client/');
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath = path.join(clientPath,'/views/');
+
+// Basic server
+
+const app = express();
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    name: 'dragons',
+    secret: 'eachcathad7kittens',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000*60*60*24*3,
+    }
+}));
+
+app.listen(2000);
+
+// Setting views
+
+app.set('view engine','ejs');
+app.set('views',viewsPath);
+
+// Routes
+
+
+app.get('/', function(req, res) {
+    console.log(req.session)
+    res.render('index', {data: req.session});
+});
+
+app.get('/famous', function(req, res) {
+    res.render('famous', {data: req.session});
+});
+
+app.post('/welcome', (req, res) => {
+    req.session.username=req.body.nombre;
+    res.send('SUCCESS');
+});
